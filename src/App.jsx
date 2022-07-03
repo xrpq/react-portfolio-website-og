8ab66969724f7343fa9864b5b0a3e1a664ab9e9c
Axel <boly.a@northeastern.edu>
2022-07-03 11:46:22 -0400

    file setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import Contact from './components/contact/contact'
+import Footer from './components/footer/footer'
+
+const app = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Portfolio />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default app
