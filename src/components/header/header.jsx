6d52483c532af6c202461c5a9d2a110892b07caf
Axel Boly <boly.a@northeastern.edu>
2022-07-03 14:24:24 -0400

    header css/jsx and some of header social jsx

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import HeaderButtons from './HeaderButtons'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Axel Boly</h1>
+        <h5 className="text-light">Aspiring Developer</h5>
+        <HeaderButtons/>
+        <HeaderSocials/>
+
+        <div className='me'>
+          <img src={ME} alta="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
