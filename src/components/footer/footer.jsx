562013f0575e2b84db7f79de011a36b9fd4a705d
Axel Boly <boly.a@northeastern.edu>
2022-07-12 00:02:02 -0400

    footer jsx

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href="#" className='footer_logo'>Axel Boly</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"> <FaFacebookF /> </a>
+        <a href="https://instagram.com"> <FiInstagram /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Axel Boly All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
