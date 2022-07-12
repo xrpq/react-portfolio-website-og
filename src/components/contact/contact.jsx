5c5b9160456345e40bd1b1c90d344c096c37ad2e
Axel Boly <boly.a@northeastern.edu>
2022-07-11 23:50:24 -0400

    contact email form and fixed background colro var

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
+import emailjs from 'emailjs-com'
 
 const contact = () => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_xqsaw4v', 'template_14mxras', form.current, 'ssbABKj0y5h1hDCie')
+ 
+    e.target.reset();
+  };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -10,8 +22,8 @@ const contact = () => {
 
       <div className="container contact__container">
         <div className="contact__options">
-          <article className="contanct__option">
-            <MdOutlineEmail />
+          <article className="contact__option">
+            <MdOutlineEmail className='contact_option-icon'/>
             <h4>Email</h4>
             <h5>bolyaxel@gmail.com</h5>
             <a href="mailto:bolyaxel@gmail.com" target="_blank">Send a message</a>
@@ -19,7 +31,7 @@ const contact = () => {
 
         </div>
         {/*END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
