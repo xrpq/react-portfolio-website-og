4a818ccf96ce57fc5ea11bab6cd549d39b5ee497
Axel Boly <boly.a@northeastern.edu>
2022-07-11 20:09:57 -0400

    abtracted portfolio jsx + media queries

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio1.jpg'
-import IMG3 from '../../assets/portfolio1.jpg'
-import IMG4 from '../../assets/portfolio1.jpg'
-import IMG5 from '../../assets/portfolio1.jpg'
-import IMG6 from '../../assets/portfolio1.jpg'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+  {
+    id: 6,
+    image: IMG1,
+    title: 'Crypto Currency Dashboard & Financial Visualization',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/shorts/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+  },
+]
 const portfolio = () => {
   return (
     <section id='portfolio'>
@@ -14,71 +53,24 @@ const portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+       {
+        data.map(({id, image, title, github, demo}) => {
+          return(
+            <article key ={id} className='portfolio__item'>
+            <div className='portfolio__item-image'>
+              <img src={image} alt={title} />
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+              <a href={github} className='btn'> Github</a>
+              <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+            </div>
+          </article>
+          )
+        })
+       }
 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
- 
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-
-        <article className='portfolio__item'>
-          <div className='portfolio__item-image'>
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'> Github</a>
-            <a href="https://dribbble.com/alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+      
       </div>
     </section>
   )
